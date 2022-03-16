ac2be09fcc518d04fdaf57cb19c9570d2f6ba038
nandriot <nikolaos.andriotis@bsc.es>
2022-03-16 21:42:36 +0100

    Updated several stuff. README, cleanup the code for exercise3, snapshots for the README

--- README.md
+++ README.md
@@ -1,9 +1,78 @@
 # gsoc-2022-warm-up
 
-## Exercise1 
+This is the repository for the warm-up exercise of Google Summer of Code 2022, for the ATHENA project titled: "HSF EIC ATHENA Clustering on GPUs"
+## Exercise1
 
--First we install the prerequisites (compilers, gpu drivers etc) for running DPCPP on our machine.
--Then we can install and launch oneapi-cli tool. After that we can generate the 2 samples for vector addition and matrix multiplication.
+First we install the prerequisites (compilers, gpu drivers etc) for running DPCPP on our machine.
+Then we can install and launch oneapi-cli tool. After that we can generate the 2 samples for vector addition and matrix multiplication.
 
+Generating the samples:
 ![alt text](https://github.com/nikosandriotis/gsoc-2022-warm-up/blob/main/snapshots/oneapicli1st.png "")
 
+![alt text](https://github.com/nikosandriotis/gsoc-2022-warm-up/blob/main/snapshots/oneapicli2ndscreenshot.png "")
+
+Building the vector sample:
+![alt text](https://github.com/nikosandriotis/gsoc-2022-warm-up/blob/main/snapshots/buildingvectoradd1.png "")
+
+![alt text](https://github.com/nikosandriotis/gsoc-2022-warm-up/blob/main/snapshots/buildingvector2.png "")
+
+And the unified shared memory version:
+![alt text](https://github.com/nikosandriotis/gsoc-2022-warm-up/blob/main/snapshots/buildingvector3.png "")
+
+![alt text](https://github.com/nikosandriotis/gsoc-2022-warm-up/blob/main/snapshots/buildingvector4.png "")
+
+
+Building the matrix sample. Here also I can see that my compiler version is outdated too:
+![alt text](https://github.com/nikosandriotis/gsoc-2022-warm-up/blob/main/snapshots/buildingmatrixmult1.png "")
+
+![alt text](https://github.com/nikosandriotis/gsoc-2022-warm-up/blob/main/snapshots/buildingmatrixmult2.png "")
+
+##Exercise2
+
+Now we check the timings if we make the size 10 times bigger and we change the device from the CPU to GPU:
+![alt text](https://github.com/nikosandriotis/gsoc-2022-warm-up/blob/main/snapshots/matrixmultwithCPUandx10.png"")
+
+![alt text](https://github.com/nikosandriotis/gsoc-2022-warm-up/blob/main/snapshots/matrixmultmultitime2nd_normaltimes10.png "")
+
+##Exercise3
+The code is in the /src directory.
+
+To run the implementation:
+
+clean the build folder:
+`cd src/build`
+`rm -rf *`
+
+Then locate your dpcpp compiler:
+
+`dpcpp --version`
+
+Then run cmake with the appropriate path:
+
+`cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/opt/intel/oneapi/compiler/2021.3.0/linux/bin/dpcpp -DCMAKE_CXX_FLAGS="-fsycl"`
+
+Then make:
+
+`make -B`
+
+And then you can run it (default size 1024, but you can change it in line 119 of src/vectMatvect.cpp):
+
+`./dpc_exercise`
+
+Here are some examples that show same results in CPU and GPU:
+![alt text](https://github.com/nikosandriotis/gsoc-2022-warm-up/blob/main/snapshots/matrixmultwithCPUandx10.png"")
+
+![alt text](https://github.com/nikosandriotis/gsoc-2022-warm-up/blob/main/snapshots/matrixmultmultitime2nd_normaltimes10.png "")
+
+##IMPORTANT NOTES
+
+Even though I was expecting VTune to be the easiest part of the assignment (I have used it extensively), it seems that I have an issue with my setup and I could not run it. Unfortunately I cannot update my versions of the compilers and drivers because this is my work's laptop.
+
+I tried on another laptop but it was not compatible since it has a Gen8 gpu (min Gen9 required for running oneapi on GPU). Here is the issue when trying to run it on GPU (I was able in CPU):
+
+![alt text](https://github.com/nikosandriotis/gsoc-2022-warm-up/blob/main/snapshots/erroronothermachine.jpg "")
+
+However what is needed in VTune is to run a gpu-offload like so:
+`vtune -collect gpu-offload -allow-multiple-runs -analyze-system -finalization-mode=full ./dpc_exercise`
+
+And then most probably with the "naive" implementation that we have here, we should check the platform schematic to see how often we missed the L3 and went to the main memory. An improvement for this would be to do Tiling and use also local identifiers in the GPU and not only the global ones. We can get much more performance by manually caching sub-blocks of the matrices (tiles) in the GPU's on-chip local memory (== shared memory in CUDA).
--- src/vectMatvect.cpp
+++ src/vectMatvect.cpp
@@ -1,24 +1,3 @@
-//==============================================================
-// Vector Add is the equivalent of a Hello, World! sample for data parallel
-// programs. Building and running the sample verifies that your development
-// environment is setup correctly and demonstrates the use of the core features
-// of DPC++. This sample runs on both CPU and GPU (or FPGA). When run, it
-// computes on both the CPU and offload device, then compares results. If the
-// code executes on both CPU and offload device, the device name and a success
-// message are displayed. And, your development environment is setup correctly!
-//
-// For comprehensive instructions regarding DPC++ Programming, go to
-// https://software.intel.com/en-us/oneapi-programming-guide and search based on
-// relevant terms noted in the comments.
-//
-// DPC++ material used in the code sample:
-// •	A one dimensional array of data shared between CPU and offload device.
-// •	A device queue and kernel.
-//==============================================================
-// Copyright © Intel Corporation
-//
-// SPDX-License-Identifier: MIT
-// =============================================================
 #include <CL/sycl.hpp>
 #include <array>
 #include <iostream>
@@ -29,10 +8,6 @@
 #endif
 
 using namespace sycl;
-
-// Array size for this example.
-size_t array_size = 4;
-
 // Create an exception handler for asynchronous SYCL exceptions
 static auto exception_handler = [](sycl::exception_list e_list) {
   for (std::exception_ptr const &e : e_list) {
@@ -47,9 +22,8 @@ static auto exception_handler = [](sycl::exception_list e_list) {
     }
   }
 };
-
 //************************************
-// Vector add in DPC++ on device: returns sum in 4th parameter "sum".
+// (v^T * b * v) DPC++ on device:
 //************************************
 void vecMatvec(queue& q,
     const int* v_host,
@@ -58,55 +32,33 @@ void vecMatvec(queue& q,
     int* res_gpu,
     int* sum,
     size_t N) {
-    /*
-        To Multiply: C[M][P] = A[M][N] * B[N][P]
-    */
     try {
-
-        //buffer<int, 1> v(v_host.data(), range<1>{v_host.size()}); // 1D vector v
-        //buffer<int, 2> b(b_host.data(), range<2>{N, N}); // 2D buffer for matrix b
-        //buffer<int, 1> c(c_gpu.data(), range<1>{c_gpu.size()}); // 1D vector intermediate
-        //buffer<int, 1> res(res_gpu.data(), range<1>{res_gpu.size()}); // 1D vector for partial results
-
         std::cout <<("Starting computing on GPU...\n");
         std::cout << "GPU::Multiplying V^T and B into C.\n";
         auto firstkernel = q.submit([&](handler& h) {
-
-            //auto V = v_host.get_access<access::mode::read>(h);
-            //auto B = b_host.get_access<access::mode::read>(h);
-            //auto C = c_gpu.get_access<access::mode::write>(h);
            std::cout << "GPU::Hola.\n";
             h.parallel_for(range<2>{N, N}, [=](id<2> index) {
-                // Threading index that iterates over c.
+                // Threading index that iterates over c_gpu.
                 int row = index[0];
                 int col = index[1];
-                //auto sum = 0;
-                // Compute result of ONE element of C
-                //for (int i = 0; i < N; i++)
+                //important here to have the unique indexing of c_gpu
                 c_gpu[row * N + col] = v_host[col] * b_host[row * N + col];
                 });
             });
 
         std::cout << "GPU::Multiplying C and V into RES.\n";
         auto secondkernel = q.submit([&](handler& h) {
-
-            //auto C = c_gpu.get_access<access::mode::read>(h);
-            //auto V = v_host.get_access<access::mode::read>(h);
-            //auto RES = res_gpu.get_access<access::mode::write>(h);
-
             h.parallel_for(N, [=](id<1> index) {
-                // Threading index that iterates over c.
+                // Threading index that iterates over res_gpu.
                 int id = index[0];
-                // Compute result of ONE element of C
+                //with the for loop here we avoid a necessary reduction here
                 for (int col = 0; col<N; col++)
                 res_gpu[id] += v_host[id] * c_gpu[id * N + col];
                 });
             });
-
         std::cout << "GPU::Reduction happening.\n";
+        //sycl reduction implementation
         auto reduction = q.submit([&](handler& h) {
-            //auto RES = res_gpu.get_access<access::mode::read>(h);
-
             h.parallel_for(N, [=](id<1> i) {
               sycl::ONEAPI::atomic_ref<int,
                                        sycl::ONEAPI::memory_order::relaxed,
@@ -123,6 +75,7 @@ void vecMatvec(queue& q,
     }
 }
 
+//initialize the objects
 void initData(int* v, int* b, size_t size){
   // initialize vector
   for (int i=0; i<size; i++) v[i] = i;
@@ -134,6 +87,7 @@ void initData(int* v, int* b, size_t size){
   }
 }
 
+//verification on CPU
 int CPU_calculation(int* v, int* b, size_t size) {
   //we have to compute (v^T * b * v)
   int expected_sum = 0.0;
@@ -144,9 +98,7 @@ int CPU_calculation(int* v, int* b, size_t size) {
   }
   return expected_sum;
 }
-//************************************
-// Demonstrate vector add both in sequential on CPU and in parallel on device.
-//************************************
+
 int main(int argc, char* argv[]) {
   // Change array_size if it was passed as argument
   if (argc > 1) array_size = std::stoi(argv[1]);
@@ -159,25 +111,21 @@ int main(int argc, char* argv[]) {
   INTEL::fpga_selector d_selector;
 #else
   // The default device selector will select the most performant device.
-  default_selector d_selector;
+  gpu_selector d_selector;
 #endif
 
   try {
-    constexpr size_t N = 4;
+    //set the size of your problem here.
+    constexpr size_t N = 1024;
 
     queue q(d_selector, exception_handler);
+
     // Print out the device information used for the kernel code.
     std::cout << "Running on device: "
               << q.get_device().get_info<info::device::name>() << "\n";
     std::cout << "N is: " << N << "\n";
 
-    // Create arrays with "array_size" to store input and output data. Allocate
-    // unified shared memory so that both CPU and device can access them.
-    //std::vector<int> v(array_size);
-    //std::vector<int> b(array_size * array_size);
-    //std::vector<int> c_gpu(array_size * array_size);
-    //std::vector<int> res_gpu(array_size);
-
+    //shared variables so we can access them from cpu and gpu
     int *v = malloc_shared<int>(N, q);
     int *b = malloc_shared<int>(N*N, q);
     int *c_gpu = malloc_shared<int>(N*N, q);
@@ -187,7 +135,7 @@ int main(int argc, char* argv[]) {
 
     initData(v, b, N);
     int expected_sum = CPU_calculation(v, b, N);
-    // Vector addition in DPC++.
+
     vecMatvec(q, v, b, c_gpu, res_gpu, sum, N);
     std::cout << "GPU_RESULT:"<< *sum << "\n";
     std::cout << "EXPECTED_RESULT:"<<expected_sum << "\n";
@@ -207,7 +155,6 @@ int main(int argc, char* argv[]) {
     std::cout << "An exception is caught while adding two vectors.\n";
     std::terminate();
   }
-
   std::cout << "Vector add successfully completed on device.\n";
   return 0;
 }
